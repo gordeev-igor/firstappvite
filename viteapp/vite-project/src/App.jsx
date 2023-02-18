68bde9ddb306564c43df7c825ec01cc35a64bd45
Igor Gordeev <gordeev070814@gmail.com>
2023-02-18 14:31:08 +0300

    12

--- viteapp/vite-project/src/App.jsx
+++ viteapp/vite-project/src/App.jsx
@@ -8,29 +8,36 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <div className="title">
-          Welcome.
-        </div>
-        <div className="subtitle">
-          Hello, nice to se you again!
-        </div>
-
-        <div className="form">
-          <div className="form__content">
-            <div className="form__content-login">
-              <p>email/username</p>
-              <form className="login">
-                <input class="input__name" type="text" placeholder="username"></input>
-              </form>
-            </div>
-            <div className="form__content-pass">
-              <p>email/username</p>
-              <form className="form__content-pass">
-                <input class="input__pass" type="password" placeholder="password"></input>
-              </form>
+        <div className="registration">
+          <div className="registration__inner">
+            <h1 className="title">
+              Welcome.
+            </h1>
+            <h3 className="subtitle">
+              Hello, nice to se you again!
+            </h3>
+            <div className="form__content">
+              <div className="form__content-inner">
+                <div className="username">
+                  <div className="username__title">
+                    email/username
+                  </div>
+                  <form className="form__login">
+                    <input class="input__name" type="text"></input>
+                  </form>
+                </div>
+                <div className="password">
+                  <div className="password__title">
+                    password
+                  </div>
+                  <form className="form__pass">
+                    <input class="input__pass" type="password"></input>
+                  </form>
+                </div>
+                <button type="submit">LOGIN</button>
+              </div>
             </div>
           </div>
-
         </div>
       </div>
     </div>
