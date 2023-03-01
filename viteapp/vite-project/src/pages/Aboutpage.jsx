5fc4289e7ff66a24ae0e073bb31bed02a71fbfa2
gordeev-igor01~ <gordeev070814@gmail.com>
2023-03-01 22:44:30 +0300

    vvv

--- viteapp/vite-project/src/pages/Aboutpage.jsx
+++ viteapp/vite-project/src/pages/Aboutpage.jsx
@@ -1,8 +1,28 @@
+import {useState , useEffect} from 'react'
+import '../App.css';
+import  axios  from 'axios';
+
+const src = "https://content.guardianapis.com/search?page=2&q=debate&api-key=test"
+
 const Aboutpage = () => {
+
+  const [articles , setArticles] = useState([]);
+  useEffect(() => {
+    axios
+      .get(src)
+      .then(data => {
+        setArticles(data.data.response.results);
+      })
+  }, [])
+
   return (
-    <p>
-      Aboutpage
-    </p>
+    <div className="Aboutpage">
+      {articles.map(article => {
+        return(
+          <p><a href={article.webUrl}>{article.webTitle}</a></p>
+        )
+      })}
+    </div>
   )
 }
 
